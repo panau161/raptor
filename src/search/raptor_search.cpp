0730ece70effdcb635a5839245e1c09bdb71ba80
Enrico Seiler <enrico.seiler@hotmail.de>
2025-03-26 17:35:31 +0100

    hardcode fpga for search

--- src/search/raptor_search.cpp
+++ src/search/raptor_search.cpp
@@ -11,6 +11,8 @@
 #include <raptor/search/search_ibf.hpp>
 #include <raptor/search/search_partitioned_ibf.hpp>
 
+void raptor_search_fpga_oneapi(raptor::search_fpga_arguments const & arguments);
+
 namespace raptor
 {
 
@@ -18,12 +20,14 @@ void raptor_search(search_arguments const & arguments)
 {
     arguments.complete_search_timer.start();
 
-    if (arguments.is_hibf)
-        search_hibf(arguments);
-    else if (arguments.parts == 1u)
-        search_ibf(arguments);
-    else
-        search_partitioned_ibf(arguments);
+    raptor_search_fpga_oneapi(raptor::search_fpga_arguments{arguments});
+
+    // if (arguments.is_hibf)
+    //     search_hibf(arguments);
+    // else if (arguments.parts == 1u)
+    //     search_ibf(arguments);
+    // else
+    //     search_partitioned_ibf(arguments);
 
     arguments.complete_search_timer.stop();
 
